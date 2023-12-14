92ce4f7ca4b119b83ab29d34df93f05c1c82a3a9
quangnv1212 <ITITIU20149@student.hcmiu.edu.vn>
2023-12-14 15:40:07 +0000

    Even closer

--- src/main.ts
+++ src/main.ts
@@ -63,22 +63,43 @@ const withBottomRightLift = (construction: Element) =>
     ]),
   ]);
 
-  const classroom = (name: string) => h("div.w-20.h-16.border.border-black")
-const rowOfClassrooms = () => h("div.grid.grid-cols-6.grid-rows-2", Array.from({ length: 12 }, () => classroom("A6.111")))
+const classroom = (name: string) =>
+  h(
+    "div.w-20.h-16.border.border-black.flex.justify-center.items-center",
+    h("span.inline-block", name)
+  );
+const rowOfClassrooms = () =>
+  h(
+    "div.grid.grid-cols-6.grid-rows-2.gap-x-2.gap-y-5",
+    Array.from({ length: 12 }, () => classroom("A6.111"))
+  );
 
 const root = document.querySelector<HTMLDivElement>("#app")!;
 root.appendChild(
-  h("div.grid.grid-cols-5.grid-rows-5.w-fit", {style:"grid-template-rows:repeat(5,auto);grid-template-columns:repeat(5,auto)"}, [
-    [YES("STAIRS"), NO(), YES("STAIRS"), NO(), NO()],
-    [YES(withTopRightLift(twoDoors("A9.100"))), rowOfClassrooms(), YES(), YES(), YES()],
-    [YES(), NO(), NO(), NO(), NO()],
+  h(
+    "div.grid.grid-cols-5.grid-rows-5.w-fit",
+    {
+      style:
+        "grid-template-rows:repeat(5,auto);grid-template-columns:repeat(5,auto)",
+    },
     [
-      YES(withBottomRightLift(twoRooms("A6.222", "A1.999"))),
-      YES(),
-      YES(),
-      YES(),
-      YES(),
-    ],
-    [YES("STAIRS"), NO(), YES("STAIRS"), NO(), NO()],
-  ])
+      [YES("STAIRS"), NO(), YES("STAIRS"), NO(), NO()],
+      [
+        YES(withTopRightLift(twoDoors("A9.100"))),
+        rowOfClassrooms(),
+        YES(),
+        YES(),
+        YES(),
+      ],
+      [YES(), NO(), NO(), NO(), NO()],
+      [
+        YES(withBottomRightLift(twoRooms("A6.222", "A1.999"))),
+        YES(),
+        YES(),
+        YES(),
+        YES(),
+      ],
+      [YES("STAIRS"), NO(), YES("STAIRS"), NO(), NO()],
+    ]
+  )
 );
